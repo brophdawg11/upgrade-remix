c3fb9f08aaa50410da8641600af1aa1df807c93a
Matt Brophy <matt@brophy.org>
2023-08-29 11:13:13 -0400

    Add support for listing versions

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
 # upgrade-remix
 
-`upgrade-remix` is a CLI utility to update all of your [Remix](https://remix.run) dependencies together.
-
-It automatically detects the relevant `remix` and `@remix-run/*` packages from your `package.json` and runs the proper commands to upgrade them. Once updated it runs a full `npm ci`/`yarn`/whatever to ensure everything is synced up.
+`upgrade-remix` is a CLI utility to update/list all of your [Remix](https://remix.run) dependencies together.
 
 **Supported Package Managers**
 
-* [`npm`](https://www.npmjs.com)
-* [`yarn`](https://yarnpkg.com)
-* [`pnpm`](https://pnpm.io)
+- [`npm`](https://www.npmjs.com)
+- [`yarn`](https://yarnpkg.com)
+- [`pnpm`](https://pnpm.io)
 
 PR's welcome to support additional package managers!
 
 ## Usage
+
+### Updating Packages
+
+`upgrade-remix` will automatically detect the relevant `remix` and `@remix-run/*` packages from your `package.json` and runs the proper commands to upgrade them. Once updated it runs a full `npm ci`/`yarn install`/etc. to ensure everything is synced up.
 
 ```bash
 # By default it will update to the `latest` tag on NPM
@@ -21,4 +23,15 @@ PR's welcome to support additional package managers!
 # Or you can specify a specific version or tag
 > npx upgrade-remix 1.16.0
 > npx upgrade-remix pre
+```
+
+### Listing Packages
+
+You can also use `upgrade-remix` to list currently installed Remix packages to ensure they are synced up and/or de-duped accordingly:
+
+```bash
+> npx upgrade-remix --list-versions
+
+# Or using the "-l" shorthand
+> npx upgrade-remix -l
 ```
--- index.js
+++ index.js
@@ -2,10 +2,8 @@
 
 const fs = require("fs");
 const path = require("path");
+const { parseArgs } = require("node:util");
 const childProcess = require("child_process");
-
-const version = process.argv[2] || "latest";
-const isLooseVersion = /^[\^~]/.test(version);
 
 const packageJsonPath = path.join(process.cwd(), "package.json");
 if (!fs.existsSync(packageJsonPath)) {
@@ -15,71 +13,118 @@ if (!fs.existsSync(packageJsonPath)) {
   );
 }
 
-console.log(
-  `Updating remix packages in "${packageJsonPath}" to version "${version}"`
-);
-const packageJson = require(packageJsonPath);
+const { args, version, implementation } = setup();
 
-const implementations = {
-  npm: {
-    install: (packages, isDev) =>
-      "npm install " +
-      (isDev ? "--save-dev " : "--save ") +
-      (isLooseVersion ? "" : "--save-exact ") +
-      packages,
-    sync: "npm ci",
-  },
-  yarn: {
-    install: (packages, isDev) =>
-      "yarn add " +
-      (isDev ? "--dev " : "") +
-      (isLooseVersion ? "" : "--exact ") +
-      packages,
-    sync: "yarn install --frozen-lockfile",
-  },
-  pnpm: {
-    install: (packages, isDev) =>
-      "pnpm add " +
-      (isDev ? "--save-dev " : "") +
-      (isLooseVersion ? "" : "--save-exact ") +
-      packages,
-    sync: "pnpm install --frozen-lockfile",
-  },
-};
-
-let implementation;
-
-if (fs.existsSync(path.join(process.cwd(), "package-lock.json"))) {
-  console.log("Found package-lock.json, using npm");
-  implementation = implementations.npm;
-} else if (fs.existsSync(path.join(process.cwd(), "yarn.lock"))) {
-  console.log("Found yarn.lock, using yarn");
-  implementation = implementations.yarn;
-} else if (fs.existsSync(path.join(process.cwd(), "pnpm-lock.yaml"))) {
-  console.log("Found pnpm-lock.yaml, using pnpm");
-  implementation = implementations.pnpm;
+if (args["list-versions"]) {
+  listVersions();
 } else {
-  throw new Error("Unsupported Package Manager");
+  upgradePackages();
 }
 
-function installUpdates(deps, isDev) {
-  const packages = Object.keys(deps)
-    .filter(
-      (k) =>
-        (k.startsWith("@remix-run/") || k === "remix") &&
-        !k.startsWith("@remix-run/v1-") &&
-        k !== "@remix-run/router"
-    )
-    .map((k) => `${k}@${version}`)
-    .join(" ");
-  const cmd = implementation.install(packages, isDev);
-  console.log(`Executing: ${cmd}`);
-  childProcess.execSync(cmd);
+function setup() {
+  const { values: args, positionals } = parseArgs({
+    options: {
+      "list-versions": {
+        type: "boolean",
+        short: "l",
+      },
+    },
+    allowPositionals: true,
+  });
+  const version = positionals[0] || "latest";
+  const implementation = getPackageManagerImplementation(version);
+  return { args, version, implementation };
 }
 
-installUpdates(packageJson.dependencies, false);
-installUpdates(packageJson.devDependencies, true);
+function getPackageManagerImplementation(v) {
+  const isLooseVersion = /^[\^~]/.test(v);
+  const implementations = {
+    npm: {
+      install: (packages, isDev) =>
+        "npm install " +
+        (isDev ? "--save-dev " : "--save ") +
+        (isLooseVersion ? "" : "--save-exact ") +
+        packages,
+      sync: "npm ci",
+      list: (package) => `npm ls ${package}`,
+    },
+    yarn: {
+      install: (packages, isDev) =>
+        "yarn add " +
+        (isDev ? "--dev " : "") +
+        (isLooseVersion ? "" : "--exact ") +
+        packages,
+      sync: "yarn install --frozen-lockfile",
+      list: (package) => `yarn list --pattern ${package}`,
+    },
+    pnpm: {
+      install: (packages, isDev) =>
+        "pnpm add " +
+        (isDev ? "--save-dev " : "") +
+        (isLooseVersion ? "" : "--save-exact ") +
+        packages,
+      sync: "pnpm install --frozen-lockfile",
+      list: (package) => `pnpm list ${package}`,
+    },
+  };
 
-const syncCmd = implementation.sync;
-console.log(`Running '${syncCmd}' to sync up all deps`);
-childProcess.execSync(syncCmd);
+  if (fs.existsSync(path.join(process.cwd(), "package-lock.json"))) {
+    console.log("Found package-lock.json, using npm");
+    return implementations.npm;
+  } else if (fs.existsSync(path.join(process.cwd(), "yarn.lock"))) {
+    console.log("Found yarn.lock, using yarn");
+    return implementations.yarn;
+  } else if (fs.existsSync(path.join(process.cwd(), "pnpm-lock.yaml"))) {
+    console.log("Found pnpm-lock.yaml, using pnpm");
+    return implementations.pnpm;
+  } else {
+    throw new Error("Unsupported Package Manager");
+  }
+}
+
+function getDeps(deps) {
+  return Object.keys(deps || {}).filter(
+    (k) =>
+      (k.startsWith("@remix-run/") || k === "remix") &&
+      !k.startsWith("@remix-run/v1-") &&
+      k !== "@remix-run/router"
+  );
+}
+
+function listVersions() {
+  console.log(`Listing remix packages in "${packageJsonPath}"`);
+  const packageJson = require(packageJsonPath);
+  let deps = [
+    ...getDeps(packageJson.dependencies),
+    ...getDeps(packageJson.devDependencies),
+  ];
+  deps.forEach((dep) => {
+    let cmd = implementation.list(dep);
+    console.log(`Executing: ${cmd}`);
+    let stdout = childProcess.execSync(cmd).toString();
+    console.log(stdout);
+  });
+}
+
+function upgradePackages() {
+  console.log(
+    `Updating remix packages in "${packageJsonPath}" to version "${version}"`
+  );
+  const packageJson = require(packageJsonPath);
+
+  function installUpdates(deps, isDev) {
+    const packages = getDeps(deps)
+      .map((k) => `${k}@${version}`)
+      .join(" ");
+    const cmd = implementation.install(packages, isDev);
+    console.log(`Executing: ${cmd}`);
+    childProcess.execSync(cmd);
+  }
+
+  installUpdates(packageJson.dependencies, false);
+  installUpdates(packageJson.devDependencies, true);
+
+  const syncCmd = implementation.sync;
+  console.log(`Running '${syncCmd}' to sync up all deps`);
+  childProcess.execSync(syncCmd);
+}
